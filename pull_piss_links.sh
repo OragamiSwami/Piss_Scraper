8c0b6582c7d3096db63f2771e09d413656a262c8
OragamiSwami <85419962+OragamiSwami@users.noreply.github.com>
2021-06-06 04:01:57 -0600

    Update pull_piss_links.sh

--- pull_piss_links.sh
+++ pull_piss_links.sh
@@ -6,17 +6,20 @@
 #notes      :Install curl and unrealircd (https://www.unrealircd.org/download)
 #usage      :pull_piss_links.sh
 
+cd "$(dirname "$0")"
+log="pull.log"
 tmp=".frampad.tmp"
 file="frampad.conf"
 unreal_dir="/home/ircd/unrealircd/"
 my_server="your.server.tld"
 list="https://mensuel.framapad.org/p/********/export/txt" #obtain URL from #oper topic
 
+echo "Start @ `date`" &>> $log
 curl -sk "$list" -o $tmp.orig
 c=`grep -c host $tmp.orig`
 cat $tmp.orig | tr -cd '[:alnum:]._\-;#":,\{\}\(\)\/\!\?\*+=@ \n'"'" | sed 's/^\*//' > $tmp
 awk '/^link/,/^}$/' $tmp > $file
 d=`grep -c hostname $file`
-if [ $d -ne $c ] && [ $d -ne `expr $c - 1` ]; then echo "Simple count check failed.. Exiting";cp $file .$file.bad; cp .$file.lng $file; exit; fi
-
-$unreal_dir/unrealircd configtest && cp $file .$file.lng || ( cp $file .$file.bad; cp .$file.lng $file )
+(if [ $d -ne $c ] && [ $d -ne `expr $c - 1` ]; then echo "Simple count check failed.. Exiting";cp $file .$file.bad; cp .$file.lng $file; exit; fi) &>> $log
+($unreal_dir/unrealircd configtest && cp $file .$file.lng && $unreal_dir/unrealircd rehash || ( cp $file .$file.bad; cp .$file.lng $file )) &>> $log
+echo -e "End @ `date`\n\n" &>> $log
