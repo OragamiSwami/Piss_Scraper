2e720d671c8521d7db80ba3d76650143a85d3384
OragamiSwami <85419962+OragamiSwami@users.noreply.github.com>
2021-06-16 10:24:25 -0600

    Update pull_piss_links.sh

--- pull_piss_links.sh
+++ pull_piss_links.sh
@@ -17,9 +17,9 @@ list="https://mensuel.framapad.org/p/********/export/txt" #obtain URL from #oper
 echo "Start @ `date`" &>> $log
 curl -sk "$list" -o $tmp.orig
 c=`grep -c host $tmp.orig`
-cat $tmp.orig | tr -cd '[:alnum:]._\-;#":,\{\}\(\)\/\!\?\*+=@ \n'"'" | sed 's/^\*//' > $tmp
+cat $tmp.orig | tr -cd '[:alnum:]._\-;#":,\{\}\(\)\/\!\?\*+=@ \n'"'" | sed 's/^ //' > $tmp
 awk '/^link/,/^}/' $tmp > $file
 d=`grep -c hostname $file`
-(if [ $d -ne $c ] && [ $d -ne `expr $c - 1` ]; then echo "Simple count check failed.. Exiting";cp $file .$file.bad; cp .$file.lng $file; exit; fi) &>> $log
-($unreal_dir/unrealircd configtest && cp $file .$file.lng && $unreal_dir/unrealircd rehash || ( cp $file .$file.bad; cp .$file.lng $file )) &>> $log
+if [ $d -ne $c ] && [ $d -ne `expr $c - 1` ] && [ $d -le 50 ] ; then echo "Simple count check failed.. Exiting" &>> $log ;cp $file .$file.bad; cp .$file.lng $file; exit; fi
+$unreal_dir/unrealircd configtest &>>$log && ( cp $file .$file.lng && $unreal_dir/unrealircd rehash &>>$log ) || cp $file .$file.bad; cp .$file.lng $file
 echo -e "End @ `date`\n\n" &>> $log
