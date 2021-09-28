f219b1a686e089eecefd1333ae02f29f1fc0c503
OragamiSwami <85419962+OragamiSwami@users.noreply.github.com>
2021-09-27 19:22:27 -0600

    Update pull_piss_links.sh
    Update to support API

--- pull_piss_links.sh
+++ pull_piss_links.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-#title      :pull_piss_links.sh
 #author     :OragamiSwami
 #date       :20210605
 #version    :0.1
@@ -10,10 +8,7 @@ cd "$(dirname "$0")"
 log="pull.log"
 hubfile="hubs.conf"
 linkfile="links.conf"
-banfile="bans.conf"
 unreal_dir="/home/ircd/unrealircd/"
-list="https://wiki.letspiss.net/index.php?title=Server_link_blocks&action=raw" #obtain URL from #oper topic
-banlist="https://wiki.letspiss.net/index.php?title=Ban_Blocks&action=raw"
 
 
 echo "Start @ `date`" &>> $log
@@ -24,24 +19,29 @@ function pull {
     file=$3
     mincount=$4
 
+    echo "Pulling $name" &>> $log
     curl -sk "$link" -o .$file.orig
     c=`grep -c host .$file.orig`
     cat .$file.orig | tr -cd '[:alnum:]._\-;#":,\{\}\(\)\/\!\?\*+=@ \n\t'"'" | sed 's/^ [^ ]//;s/\t/    /g;s/autoconnect\s*;//' > .$file.tmp
     awk '/^(link|ban) /,/^}/' .$file.tmp > $file
     d=`grep -c hostname $file`
+    echo "Got $d links for $name" &>> $log
     if [ $d -ne $c ] && [ $d -ne `expr $c - 1` ] && [ $d -le $mincount ] ; then
         echo "$name count check failed.. Restoring LNG" &>> $log
         cp $file .$file.bad
         cp .$file.lng $file || ( echo "Failed to restore LNG" &>> $log; return -1)
     fi
-    $unreal_dir/unrealircd configtest &>>$log && cp $file .$file.lng || ( cp $file .$file.bad; cp .$file.lng $file )
+    $unreal_dir/unrealircd configtest &>> $log && cp $file .$file.lng || ( cp $file .$file.bad; cp .$file.lng $file )
+    echo "End $name" &>> $log
 }
 
 
 
-pull "Hubs" "$list&section=8" $hubfile 10
-pull "Link" "$list&section=9" $linkfile 70
-pull "Ban" "$banlist" $banfile 2
+pull "Hubs" "https://api.shitposting.space/link/hubs" $hubfile 10
+pull "Americas" "https://api.shitposting.space/link/leaves/America" $linkfile 20
+#pull "Europe" "https://api.shitposting.space/link/leaves/Europe" $linkfile 20
+#pull "APAC" "https://api.shitposting.space/link/leaves/APAC" $linkfile 11
+#pull "Africa" "https://api.shitposting.space/link/leaves/Africa" $linkfile 1
 
 sed -i 's/class servers;/class hubs;/' $hubfile
 
